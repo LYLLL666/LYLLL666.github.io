ed6649977b065476f6c1889bd8b31efc055d84af
LYLLL666 <2667849583@qq.com>
2026-05-15 17:05:53 +0800

    Update chat.js

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,29 +1,12 @@
-export async function onRequestPost(context) {
+export async function onRequestPost() {
 
-  const body = await context.request.json();
-
-  const res = await fetch("https://api.deepseek.com/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": "sk-1f50671c6d4347acba71ae2b2e0e9081"
-    },
-    body: JSON.stringify({
-      model: "deepseek-chat",
-      messages: [
-        {
-          role: "user",
-          content: body.message
-        }
-      ]
-    })
-  });
-
-  const data = await res.json();
-
-  return new Response(JSON.stringify(data), {
+  return new Response(JSON.stringify({
+    ok: true,
+    msg: "Functions is working"
+  }), {
     headers: {
       "Content-Type": "application/json"
     }
   });
+
 }
